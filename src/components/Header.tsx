014a3ac454fd44a82b0e9f754eec45b2eaecab94
Alysson Trabalho <alysson.bormann@gmail.com>
2023-01-20 11:07:43 -0400

    create summaryTable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,7 @@ export function Header(){
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={logoImage} alt="Habits"/>
         <button type="button" className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300">
-          Novo hábito 1:12:06
-          https://www.youtube.com/watch?v=HvcGBrIe_5o&ab_channel=Rocketseat
+          Novo hábito
           <Plus size={20} className="text-violet-500"/>
         </button>
     </div>
